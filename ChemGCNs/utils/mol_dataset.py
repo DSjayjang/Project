ac3199b6f688acf6463323579b391b9154864758
DSjayjang <a52071194@gmail.com>
2025-08-29 18:54:51 +0900

    chem

--- ChemGCNs/utils/mol_dataset.py
+++ ChemGCNs/utils/mol_dataset.py
@@ -118,61 +118,99 @@ class MoleculeDataset_esol:
 
             # 1
             graph_obj.MolLogP = dsc.MolLogP(graph_obj.mol)
-            graph_obj.SMR_VSA10 = dsc.SMR_VSA10(graph_obj.mol)
-            graph_obj.MaxEStateIndex = dsc.MaxEStateIndex(graph_obj.mol)
             graph_obj.MaxAbsPartialCharge = dsc.MaxAbsPartialCharge(graph_obj.mol)
-            graph_obj.BCUT2D_CHGHI = dsc.BCUT2D_CHGHI(graph_obj.mol)
+            graph_obj.MaxEStateIndex = dsc.MaxEStateIndex(graph_obj.mol)
+            graph_obj.SMR_VSA10 = dsc.SMR_VSA10(graph_obj.mol)
+            graph_obj.Kappa2 = dsc.Kappa2(graph_obj.mol)
             # 6
             graph_obj.BCUT2D_MWLOW = dsc.BCUT2D_MWLOW(graph_obj.mol)
-            graph_obj.fr_imide = dsc.fr_imide(graph_obj.mol)
-            graph_obj.Kappa2 = dsc.Kappa2(graph_obj.mol)
+            graph_obj.PEOE_VSA13 = dsc.PEOE_VSA13(graph_obj.mol)
             graph_obj.MinAbsPartialCharge = dsc.MinAbsPartialCharge(graph_obj.mol)
-            graph_obj.NumAromaticHeterocycles = dsc.NumAromaticHeterocycles(graph_obj.mol)
+            graph_obj.BCUT2D_CHGHI = dsc.BCUT2D_CHGHI(graph_obj.mol)
+            graph_obj.PEOE_VSA6 = dsc.PEOE_VSA6(graph_obj.mol)
             # 11
             graph_obj.SlogP_VSA1 = dsc.SlogP_VSA1(graph_obj.mol)
-            graph_obj.fr_amide = dsc.fr_amide(graph_obj.mol)
+            graph_obj.fr_nitro = dsc.fr_nitro(graph_obj.mol)
             graph_obj.BalabanJ = dsc.BalabanJ(graph_obj.mol)
-            graph_obj.fr_Ar_NH = dsc.fr_Ar_NH(graph_obj.mol)
-            graph_obj.PEOE_VSA8 = dsc.PEOE_VSA8(graph_obj.mol)
-            # 16
-            graph_obj.NumSaturatedRings = dsc.NumSaturatedRings(graph_obj.mol)
-            graph_obj.fr_NH0 = dsc.fr_NH0(graph_obj.mol)
-            graph_obj.PEOE_VSA13 = dsc.PEOE_VSA13(graph_obj.mol)
-            graph_obj.fr_barbitur = dsc.fr_barbitur(graph_obj.mol)
-            graph_obj.fr_alkyl_halide = dsc.fr_alkyl_halide(graph_obj.mol)
-            # 21
-            graph_obj.fr_C_O = dsc.fr_C_O(graph_obj.mol)
-            graph_obj.fr_bicyclic = dsc.fr_bicyclic(graph_obj.mol)
-            graph_obj.fr_ester = dsc.fr_ester(graph_obj.mol)
-            graph_obj.PEOE_VSA9 = dsc.PEOE_VSA9(graph_obj.mol)
-            graph_obj.fr_Al_OH_noTert = dsc.fr_Al_OH_noTert(graph_obj.mol)
-            # 26
-            graph_obj.SlogP_VSA10 = dsc.SlogP_VSA10(graph_obj.mol)
-            graph_obj.EState_VSA11 = dsc.EState_VSA11(graph_obj.mol)
-            graph_obj.fr_imidazole = dsc.fr_imidazole(graph_obj.mol)
-            graph_obj.EState_VSA10 = dsc.EState_VSA10(graph_obj.mol)
-            graph_obj.EState_VSA5 = dsc.EState_VSA5(graph_obj.mol)
-            # 31
             graph_obj.SMR_VSA9 = dsc.SMR_VSA9(graph_obj.mol)
-            graph_obj.FractionCSP3 = dsc.FractionCSP3(graph_obj.mol)
-            graph_obj.FpDensityMorgan2 = dsc.FpDensityMorgan2(graph_obj.mol)
-            graph_obj.fr_furan = dsc.fr_furan(graph_obj.mol)
+            graph_obj.fr_alkyl_halide = dsc.fr_alkyl_halide(graph_obj.mol)
+            # 16
             graph_obj.fr_hdrzine = dsc.fr_hdrzine(graph_obj.mol)
-            # 36
+            graph_obj.PEOE_VSA8 = dsc.PEOE_VSA8(graph_obj.mol)
+            graph_obj.fr_Ar_NH = dsc.fr_Ar_NH(graph_obj.mol)
+            graph_obj.fr_imidazole = dsc.fr_imidazole(graph_obj.mol)
+            graph_obj.fr_Nhpyrrole = dsc.fr_Nhpyrrole(graph_obj.mol)
+            # 21
+            graph_obj.EState_VSA5 = dsc.EState_VSA5(graph_obj.mol)
+            graph_obj.PEOE_VSA4 = dsc.PEOE_VSA4(graph_obj.mol)
+            graph_obj.fr_ester = dsc.fr_ester(graph_obj.mol)
+            graph_obj.PEOE_VSA2 = dsc.PEOE_VSA2(graph_obj.mol)
+            graph_obj.NumAromaticCarbocycles = dsc.NumAromaticCarbocycles(graph_obj.mol)
+            # 26
+            graph_obj.BCUT2D_LOGPHI = dsc.BCUT2D_LOGPHI(graph_obj.mol)
+            graph_obj.EState_VSA11 = dsc.EState_VSA11(graph_obj.mol)
+            graph_obj.fr_furan = dsc.fr_furan(graph_obj.mol)
+            graph_obj.EState_VSA2 = dsc.EState_VSA2(graph_obj.mol)
+            graph_obj.fr_benzene = dsc.fr_benzene(graph_obj.mol)
+            # 31
+            graph_obj.fr_sulfide = dsc.fr_sulfide(graph_obj.mol)
             graph_obj.fr_aryl_methyl = dsc.fr_aryl_methyl(graph_obj.mol)
+            graph_obj.SlogP_VSA10 = dsc.SlogP_VSA10(graph_obj.mol)
+            graph_obj.HeavyAtomMolWt = dsc.HeavyAtomMolWt(graph_obj.mol)
+            graph_obj.fr_nitro_arom_nonortho = dsc.fr_nitro_arom_nonortho(graph_obj.mol)
+            # 36
+            graph_obj.FpDensityMorgan2 = dsc.FpDensityMorgan2(graph_obj.mol)
             graph_obj.EState_VSA8 = dsc.EState_VSA8(graph_obj.mol)
-            graph_obj.fr_phos_acid = dsc.fr_phos_acid(graph_obj.mol)
+            graph_obj.fr_bicyclic = dsc.fr_bicyclic(graph_obj.mol)
+            graph_obj.fr_aniline = dsc.fr_aniline(graph_obj.mol)
+            graph_obj.fr_allylic_oxid = dsc.fr_allylic_oxid(graph_obj.mol)
+            # 41
+            graph_obj.fr_C_S = dsc.fr_C_S(graph_obj.mol)
             graph_obj.SlogP_VSA7 = dsc.SlogP_VSA7(graph_obj.mol)
             graph_obj.SlogP_VSA4 = dsc.SlogP_VSA4(graph_obj.mol)
-            # 41
-            graph_obj.EState_VSA2 = dsc.EState_VSA2(graph_obj.mol)
-            graph_obj.fr_nitro_arom_nonortho = dsc.fr_nitro_arom_nonortho(graph_obj.mol)
             graph_obj.fr_para_hydroxylation = dsc.fr_para_hydroxylation(graph_obj.mol)
+            graph_obj.PEOE_VSA7 = dsc.PEOE_VSA7(graph_obj.mol)
+            # 46
+            graph_obj.fr_Al_OH_noTert = dsc.fr_Al_OH_noTert(graph_obj.mol)
+            graph_obj.fr_pyridine = dsc.fr_pyridine(graph_obj.mol)
+            graph_obj.fr_phos_acid = dsc.fr_phos_acid(graph_obj.mol)
+            graph_obj.fr_phos_ester = dsc.fr_phos_ester(graph_obj.mol)
+            graph_obj.NumAromaticHeterocycles = dsc.NumAromaticHeterocycles(graph_obj.mol)
+            # 51
+            graph_obj.EState_VSA7 = dsc.EState_VSA7(graph_obj.mol)
+            graph_obj.PEOE_VSA12 = dsc.PEOE_VSA12(graph_obj.mol)
+            graph_obj.Ipc = dsc.Ipc(graph_obj.mol)
+            graph_obj.FpDensityMorgan1 = dsc.FpDensityMorgan1(graph_obj.mol)
+            graph_obj.PEOE_VSA14 = dsc.PEOE_VSA14(graph_obj.mol)
+            # 56
+            graph_obj.fr_guanido = dsc.fr_guanido(graph_obj.mol)
+            graph_obj.fr_benzodiazepine = dsc.fr_benzodiazepine(graph_obj.mol)
+            graph_obj.fr_thiophene = dsc.fr_thiophene(graph_obj.mol)
+            graph_obj.fr_Ndealkylation1 = dsc.fr_Ndealkylation1(graph_obj.mol)
+            graph_obj.fr_aldehyde = dsc.fr_aldehyde(graph_obj.mol)
+            # 61
+            graph_obj.fr_term_acetylene = dsc.fr_term_acetylene(graph_obj.mol)
+            graph_obj.SMR_VSA2 = dsc.SMR_VSA2(graph_obj.mol)
+            graph_obj.fr_lactone = dsc.fr_lactone(graph_obj.mol)
             
             self.data.append((graph_obj, tgt))
             self.graphs.append(graph_obj)
 
-        for feat in ['MolLogP','SMR_VSA10','MaxEStateIndex','MaxAbsPartialCharge','BCUT2D_CHGHI','BCUT2D_MWLOW','fr_imide','Kappa2','MinAbsPartialCharge','NumAromaticHeterocycles','SlogP_VSA1','fr_amide','BalabanJ','fr_Ar_NH','PEOE_VSA8','NumSaturatedRings','fr_NH0','PEOE_VSA13','fr_barbitur','fr_alkyl_halide','fr_C_O','fr_bicyclic','fr_ester','PEOE_VSA9','fr_Al_OH_noTert','SlogP_VSA10','EState_VSA11','fr_imidazole','EState_VSA10','EState_VSA5','SMR_VSA9','FractionCSP3','FpDensityMorgan2','fr_furan','fr_hdrzine','fr_aryl_methyl','EState_VSA8','fr_phos_acid','SlogP_VSA7','SlogP_VSA4','EState_VSA2','fr_nitro_arom_nonortho','fr_para_hydroxylation']:
+        for feat in ['MolLogP', 'MaxAbsPartialCharge', 'MaxEStateIndex', 'SMR_VSA10',
+       'Kappa2', 'BCUT2D_MWLOW', 'PEOE_VSA13', 'MinAbsPartialCharge',
+       'BCUT2D_CHGHI', 'PEOE_VSA6', 'SlogP_VSA1', 'fr_nitro', 'BalabanJ',
+       'SMR_VSA9', 'fr_alkyl_halide', 'fr_hdrzine', 'PEOE_VSA8', 'fr_Ar_NH',
+       'fr_imidazole', 'fr_Nhpyrrole', 'EState_VSA5', 'PEOE_VSA4', 'fr_ester',
+       'PEOE_VSA2', 'NumAromaticCarbocycles', 'BCUT2D_LOGPHI', 'EState_VSA11',
+       'fr_furan', 'EState_VSA2', 'fr_benzene', 'fr_sulfide', 'fr_aryl_methyl',
+       'SlogP_VSA10', 'HeavyAtomMolWt', 'fr_nitro_arom_nonortho',
+       'FpDensityMorgan2', 'EState_VSA8', 'fr_bicyclic', 'fr_aniline',
+       'fr_allylic_oxid', 'fr_C_S', 'SlogP_VSA7', 'SlogP_VSA4',
+       'fr_para_hydroxylation', 'PEOE_VSA7', 'fr_Al_OH_noTert', 'fr_pyridine',
+       'fr_phos_acid', 'fr_phos_ester', 'NumAromaticHeterocycles',
+       'EState_VSA7', 'PEOE_VSA12', 'Ipc', 'FpDensityMorgan1', 'PEOE_VSA14',
+       'fr_guanido', 'fr_benzodiazepine', 'fr_thiophene', 'fr_Ndealkylation1',
+       'fr_aldehyde', 'fr_term_acetylene', 'SMR_VSA2', 'fr_lactone']:
             FeatureNormalization(self.graphs, feat)
 
     def __len__(self):
