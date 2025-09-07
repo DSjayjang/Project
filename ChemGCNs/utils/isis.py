b56ceeb06ec6791442b8751f970b45ba9a81ba04
DSjayjang <a52071194@gmail.com>
2025-09-07 16:24:25 +0900

    logvp

--- ChemGCNs/utils/isis.py
+++ ChemGCNs/utils/isis.py
@@ -14,10 +14,8 @@ r('options(warn=-1)')
 hyperparams
 """
 
-# 40개 >> 28개: 성능 X
-
 nfolds = FloatVector([10])[0]
-nsis = FloatVector([40])[0]
+nsis = FloatVector([30])[0]
 seed = FloatVector([SEED])[0]
 
 family: str = 'gaussian' # gaussian / binomial / poisson / cox
@@ -55,7 +53,7 @@ class ISIS:
             tune = tune,
             penalty = penalty,
             nfolds = nfolds,
-            # nsis = nsis,
+            nsis = nsis,
             varISIS = varISIS,
             seed = seed, q = q,
             standardize = standardize)
